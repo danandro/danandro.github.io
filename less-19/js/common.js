3a1aca39254348227682fe2a6a60e6ac47cd41af
danandro <32392120+danandro@users.noreply.github.com>
2017-10-11 12:44:18 +0500

    Add files via upload

---
+++ less-19/js/common.js
@@ -0,0 +1,9 @@
+<!-- Tabs -->
+<script>
+  $('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+</script>
+
+<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
